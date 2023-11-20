186584f92e7204db3b9a7716dbb17f81acf36d39
Nyong Faal Anthony <nyongfaalanthony@gmail.com>
2023-11-20 09:18:38 +0000

    update controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -7,13 +7,13 @@ const todoControllers = {
             const { rows } = await postgre.query("select * from todo")
             res.json({msg: "OK", data: rows})
         } catch (error) {
-            res.json({msg: error.msg})
+            res.json({ msg: error.message });
         }
     },
    
     createTodo: async(req, res) => {
         try {
-            const { name, price } = req.body
+            const { title, description } = req.body
 
             const sql = 'INSERT INTO todo(title, description) VALUES($1, $2) RETURNING *'
 
@@ -22,7 +22,7 @@ const todoControllers = {
             res.json({msg: "OK", data: rows[0]})
 
         } catch (error) {
-            res.json({msg: error.msg})
+            res.json({ msg: error.message });
         }
     },
     updateTodo: async(req, res) => {
@@ -36,7 +36,7 @@ const todoControllers = {
             res.json({msg: "OK", data: rows[0]})
 
         } catch (error) {
-            res.json({msg: error.msg})
+            res.json({ msg: error.message });
         }
     },
 
@@ -55,7 +55,7 @@ const todoControllers = {
             
 
         } catch (error) {
-            res.json({msg: error.msg})
+            res.json({ msg: error.message });
         }
     }
 }
